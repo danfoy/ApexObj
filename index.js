f25503b0cb4b31ea6bb6ee764a113fcfa82b00f1
danfoy <contact@danfoy.com>
2022-01-28 01:00:16 +0000

    feat: package exports ApexObj instance
    The `index.js` file now exports a new instance of the ApexObj class.
    This allows the module to simulate a singleton (due to how nodejs caches
    modules) which will keep disk reads and blocking to a minimum.
    
    Resolves #2

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const ApexSeason = require('./classes/Season');
-const seasonData = require('./data/seasons.json').seasons[0];
+const ApexObj = require('./classes/ApexObj');
+const data = require('./data/seasons.json');
 
-const testApexObj = new ApexSeason(seasonData);
-
-console.log('Season:', testApexObj);
-console.log('Playlist 1:', testApexObj.playlists[0]);
+/**
+ * The module exports a new instance of ApexObj. This causes the module to act
+ * like a singleton due to the way nodejs caching works - as long as every
+ * consumer of this module within a given package is using the same version of
+ * it, each will receive the same instance of ApexObj.
+ *
+ * I have chosen this approach because of the way the module imports data from
+ * an external JSON file. Exporting a faux singleton massively cuts down on
+ * file reads and blocking.
+ */
+module.exports = new ApexObj(data);
