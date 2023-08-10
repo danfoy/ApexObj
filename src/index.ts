61f27b5311381e9ea3005cdf47e275640ab333f9
danfoy <contact@danfoy.com>
2023-08-10 01:47:41 +0100

    chore: fix malformed comment
    A jsdoc comment had a `w` appended to the end of the first line.

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import ApexObj from './classes/ApexObj.js';
 import seasonsData from './data/seasons.json' assert { type: 'json' };
 import legendsData from './data/legends.json' assert { type: 'json' };
 
-/**w
+/**
  * The module exports a new instance of ApexObj. This causes the module to act
  * like a singleton due to the way nodejs caching works - as long as every
  * consumer of this module within a given package is using the same version of
